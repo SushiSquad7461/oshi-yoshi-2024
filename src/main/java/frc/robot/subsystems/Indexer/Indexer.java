3886b178d57e33f1e32f54f1928d5c873e61a94d
ElaineMinaj <elainejiang1008@gmail.com>
2024-02-07 17:39:12 -0800

    indexer code

--- src/main/java/frc/robot/subsystems/Indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer/Indexer.java
@@ -5,6 +5,7 @@ import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.util.Direction;
