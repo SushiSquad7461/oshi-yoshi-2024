ea828e47f33d5c84f1122e29bb5f92268f27df2b
lodhasid <siddhant.lodha6@gmail.com>
2024-02-09 17:39:21 -0800

    elevator code

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -8,7 +8,6 @@ import SushiFrcLib.SmartDashboard.TunableNumber;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.Constants.Manipulator;
 import frc.robot.util.Direction;
