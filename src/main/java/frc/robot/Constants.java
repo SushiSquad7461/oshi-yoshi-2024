3580a4db7c2baee7c77af2cb417bdeb4f1c3852a
ElaineMinaj <elainejiang1008@gmail.com>
2024-02-13 18:08:33 -0800

    new stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -166,7 +166,7 @@ public final class Constants {
         }
 
         public static final class Indexer {
-                public static final int BEAMBREAK = 1;;
+                public static final int BEAM_BREAK = 1;;
                 public static final double UPRIGHT_ROLLERS_SPEED = 0.9;
                 public static final double INDEXER_SPEED = 0.9;
                 public static final MotorConfig INDEXER_CONFIG = new MotorConfig(
